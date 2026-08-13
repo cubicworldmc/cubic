4196a29601fe2ae81ecfc884183742d3aeb0049b
Jenya705 <jenya705@proton.me>
2026-08-13 14:18:17 +0200

    awg

--- hosts/cubic/amneziawg.nix
+++ hosts/cubic/amneziawg.nix
@@ -5,6 +5,15 @@
   pkgs,
   ...
 }:
+let
+  confPath = "/run/awg0-final.conf";
+  additionalInterface = ''
+    Table = 52111
+
+    PostUp = ip rule add priority 100 gid 30000 table 52111
+    PreDown = ip rule del priority 100 gid 30000 table 52111
+  '';
+in
 {
   config.age.secrets.amneziawg-conf-cubic = {
     file = culib.secretPath "amneziawg-conf-cubic.age";
@@ -12,24 +21,26 @@
     group = "root";
   };
 
-  config.networking.wg-quick.interfaces.wg0 = {
-    type = "amneziawg";
-    configFile = config.age.secrets.amneziawg-conf-cubic.path;
+  config.systemd.services.amneziawg-conf-setup = {
+    before = [ "wg-quick-awg0.service" ];
+    wantedBy = [ "wg-quick-awg0.service" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+    };
+
+    script = ''
+      to_ins = "${additionalInterface}"
+      ${pkgs.coreutils}/bin/cp ${config.age.secrets.amneziawg-conf-cubic.path} ${confPath}
+      ${pkgs.gnused}/bin/sed -i "s#@ADDITIONAL_INTERFACE@#$to_ins" "${confPath}"
+    '';
   };
 
-  config.networking.nftables.enable = true;
-
-  config.networking.nftable.ruleset = ''
-    table inet vpn {
-      chain output {
-        type route hook output priority mangle;
-        meta skuid {
-          1001,
-          1002
-        } meta mark set 0x100;
-      }  
-    }
-  '';
+  config.networking.wg-quick.interfaces.awg0 = {
+    type = "amneziawg";
+    configFile = confPath;
+  };
 
   config.networking.firewall.checkReversePath = lib.mkForce false;
 }
